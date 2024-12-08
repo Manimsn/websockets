44adf89d14f103a4359e8ced5c1545c27b19bbb9
mk06005@gmail.com <mk065005@gmail.com>
2024-12-08 21:45:56 +0530

    Adding interview q-1

--- client/JS-interviewQuestion/index.js
+++ client/JS-interviewQuestion/index.js
@@ -1,1 +1,7 @@
-checking
+// Question 1:
+const str = "abc" + +"def";
+console.log(str)
+
+// Unary operator tries to convert the values to a number right after it comes which in this case is a string so it cant convert to Number
+// so it returns "NaN" and the middle "+" is a binary operator cancatenates the reulst and 
+// the output is "abcNaN"
