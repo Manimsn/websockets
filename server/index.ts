a17c51ec5d03791e13e60a433de9ae92f485bcbd
Manishmsn <mk065005@gmail.com>
2024-12-30 21:05:41 +0530

    Adding - import for socket

--- server/index.ts
+++ server/index.ts
@@ -3,6 +3,7 @@ const http = require('http')
 const app = express()
 const server = http.createServer(app)
 
+import { Server } from 'socket.io'
 const io = new Server(server, {
   cors: {
     origin: '*',
